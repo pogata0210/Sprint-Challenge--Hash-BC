68a69344f658d46b353ea0e612fe005349a7ce41
Pablo Ogata <pabloogata@gmail.com>
2019-11-01 12:07:16 -0400

    first ticket

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -14,22 +14,16 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
-    route = []
-
-    # make hash table
+    route = [None] * length
+    
+    #initialize hashtable
     for ticket in tickets:
         hash_table_insert(hashtable, ticket.source, ticket.destination)
-
-    # find the first ticket
+        
+    # function to find the first ticket
     for ticket in tickets:
-        if ticket.source == 'NONE':
-            route.append(ticket.destination)
-            break
-    
-    while len(route) < length:
-        next_flight = hash_table_retrieve(hashtable, route[-1])
-        route.append(next_flight)
-
-    return route
+        if ticket.source == 'NONE': #if none there is no starting point. Therefore it must be the starting point.
+            route.append(ticket.destination) #add to list.
+            break          
 
     return route
